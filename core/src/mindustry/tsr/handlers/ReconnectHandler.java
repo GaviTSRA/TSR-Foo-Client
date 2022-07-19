adbb3793ff0b30ad300b82dc1253d44fa64efa52
GaviTSRA <gavitsra@gmail.com>
2022-07-19 18:43:09 +0200

    Fixed 'Already connected'

--- core/src/mindustry/tsr/handlers/ReconnectHandler.java
+++ core/src/mindustry/tsr/handlers/ReconnectHandler.java
@@ -7,6 +7,7 @@ public class ReconnectHandler {
     public static void reconnect() {
         String ip = Core.settings.getString("currentserverip");
         int port = Core.settings.getInt("currentserverport");
+        NetClient.disconnect();
         NetClient.connect(ip, port);
     }
 }
