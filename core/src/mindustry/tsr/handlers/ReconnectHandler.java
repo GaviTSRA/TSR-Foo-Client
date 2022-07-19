9c3797b86fee6a23e151276f8c259569b75753d5
GaviTSRA <gavitsra@gmail.com>
2022-07-19 18:43:57 +0200

    bruh

--- core/src/mindustry/tsr/handlers/ReconnectHandler.java
+++ core/src/mindustry/tsr/handlers/ReconnectHandler.java
@@ -3,11 +3,13 @@ package mindustry.tsr.handlers;
 import arc.Core;
 import mindustry.core.NetClient;
 
+import static mindustry.Vars.*;
+
 public class ReconnectHandler {
     public static void reconnect() {
         String ip = Core.settings.getString("currentserverip");
         int port = Core.settings.getInt("currentserverport");
-        NetClient.disconnect();
-        NetClient.connect(ip, port);
+        netClient.disconnectQuietly();
+        netClient.connect(ip, port);
     }
 }
